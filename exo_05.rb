7bbe4261a8640ee00bf40fc035c80b81809a58ec
Votre Nom <Vous@exemple.com>
2019-01-03 20:26:35 +0100

    second commit

---
+++ exo_05.rb
@@ -0,0 +1,45 @@
+puts "On va compter le nombre d'heures de travail à THP"
+puts "Travail : #{10 * 5 * 11}" # Valeurs - 10/5/11 - On multiplie 10 (heures) x 5 (jours) x 11 (semaines)
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}" #idem qu'au dessus x 60 (minutes)
+
+puts "Et en secondes ?"
+
+puts 10 * 5 * 11 * 60 * 60 #ajout des secondes sur une ligne séparée
+
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+
+puts 3 + 2 < 5 - 7
+
+puts "Ça fait combien 3 + 2 ? #{3 + 2}" #somme des valeurs
+puts "Ça fait combien 5 - 7 ? #{5 - 7}" #somme des valeurs
+
+puts "Ok, c'est faux alors !"
+
+puts "C'est drôle ça, faisons-en plus :"
+
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}" #bolleen
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}" #bolleen
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}" #bolleen
+
+#question : 
+
+#Pour créer une table de hashage, on utilise des accolades {}
+#les accolades = somme des valeurs
+
+=begin
+Sur la console : 
+On va compter le nombre d'heures de travail à THP
+Travail : 550
+En minutes ça fait : 33000
+Et en secondes ?
+1980000
+Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?
+false
+Ça fait combien 3 + 2 ? 5
+Ça fait combien 5 - 7 ? -2
+Ok, c'est faux alors !
+C'est drôle ça, faisons-en plus :
+Est-ce que 5 est plus grand que -2 ? true
+Est-ce que 5 est supérieur ou égal à -2 ? true
+Est-ce que 5 est inférieur ou égal à -2 ? false
+=end
---
+++ exo_07_a.rb
@@ -0,0 +1,11 @@
+puts "Bonjour, c'est quoi ton blase ?"
+user_name = gets.chomp
+puts user_name
+
+=begin
+- la méthode gets permet de demander une saisie à l'utilisateur. 
+- gets.chomp renvoie une chaîne de caractères.
+- La console indique seulement la phrase "Bonjour, c'est quoi ton blase ?"
+- L'utilisateur peut indiquer son nom en dessous
+= end
+
